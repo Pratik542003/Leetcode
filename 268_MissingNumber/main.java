3a1eb9661947dd679c0d069ff914cf1ad87d49ec
Pratik <dhanepratik543@gmail.com>
2024-02-20 11:32:39 +0530

    268 Missing Number

---
+++ 268_MissingNumber/main.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length+1;
+        int sum = (n*(n-1)/2);
+        int sum2 = 0;
+        for(int i=0;i<n-1;i++){
+            sum2 = sum2+nums[i];
+        }
+    return sum-sum2;
+    }
+}
